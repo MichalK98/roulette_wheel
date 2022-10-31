289096b86eb39d4a2bbdbab103bfff3b3cead7c7
MichalK98 <michal10203040@gmail.com>
2022-10-31 09:24:22 +0100

    Roulette Repeater with new image

--- app/index.js
+++ app/index.js
@@ -1,30 +1,69 @@
+// Elements
 const wheel = document.querySelector('#wheel');
+const timer = document.querySelector('#timer');
 
+// Wheel props
 const wheelParts = 15;
 const wheelPartsArray = [];
 const wheelSections = 1500 / wheelParts;
-const wheelRandom = Math.random() * 1500;
+const wheelStart = 8 * 1500;
 
 for (let i = 0; i < wheelParts; i++) {
   wheelPartsArray.push(i * wheelSections);
 }
 
-const delay = (time) => {
+// Delays
+const delatSpin = 12000;
+const delayClosest = 2000;
+const delayRestart = 10000;
+const delayBetween = 1000;
+
+const delay = (time, update = false) => {
+  update && updateTimer(time);
   return new Promise((resolve) => setTimeout(resolve, time));
 };
 
-const spinToRandom = async () => {
-  wheel.style.backgroundPositionX = `${wheelRandom}px`;
-  await delay(3000);
-  spinToClosest();
+const updateTimer = async (time) => {
+  for (let i = 0; i < time / 1000; i++) {
+    const secondsLeft = (time - i * 1000) / 1000;
+    timer.innerHTML = secondsLeft;
+    await delay(1000);
+  }
+  timer.innerHTML = 0;
 };
 
-const spinToClosest = () => {
+const spinWheel = async (position, speed) => {
+  wheel.style.transition = `${speed}ms cubic-bezier(0.2, 0, 0.03, 1)`;
+  wheel.style.backgroundPositionX = `${position}px`;
+  await delay(speed + delayBetween);
+};
+
+const spinToRandom = async () => {
+  const wheelRandom = Math.random() * 1500;
+  await spinWheel(wheelRandom, delatSpin);
+  spinToClosest(wheelRandom);
+};
+
+const spinToClosest = async (wheelRandom) => {
   const closest = wheelPartsArray.reduce((prev, curr) => {
     return Math.abs(curr - wheelRandom) < Math.abs(prev - wheelRandom) ? curr : prev;
   });
 
-  wheel.style.backgroundPositionX = `${closest}px`;
+  await spinWheel(closest, delayClosest);
+  restartWheel(closest);
 };
 
-spinToRandom();
+const restartWheel = async (closest) => {
+  await spinWheel(closest + wheelStart, 0);
+  await delay(delayRestart, true);
+  spinToRandom();
+};
+
+const initialStart = async () => {
+  await spinWheel(wheelStart, 0);
+  spinToRandom();
+};
+
+initialStart();
+
+// Bug wheel go to front if in middle? Change random between to test every case
